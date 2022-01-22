8fd6dc0c8676742ee04d7ee222d27cb73d8afbbe
Gabriel <gabriel.jnc@outlook.com>
2022-01-21 21:51:15 -0300

    📌 Almost Finished

--- Ordered Vector/Vetor Ordenado.py
+++ Ordered Vector/Vetor Ordenado.py
@@ -34,15 +34,40 @@ class Vetor_Ordenado:
             if i == self.ultima_posicao: # Se passar por todos elementos e o i == ultima_posicao, deve-se colocar o elemento na frente
                 posicao = i + 1 
 
-        ultima_posicao_vc = self.ultima_posicao # Variável de controle da ultima posição
+        ultima_posicao = self.ultima_posicao # Variável de controle da ultima posição
 
         # Percorrer de tras pra frente
-        while ultima_posicao_vc >= posicao: # *posição se refere a posição no qual o valor elemento vai ser inserido
-            self.vetor[ultima_posicao_vc + 1] = self.vetor[ultima_posicao_vc] # Remanejar para uma casa a frente
-            ultima_posicao_vc -= 1
+        while ultima_posicao >= posicao: # a ultima_posicao tem que ser menor do que a posicao(local onde numero será inserido) para abrir espaço para o numero ser inserido
+            self.vetor[ultima_posicao + 1] = self.vetor[ultima_posicao] # Remanejar para uma casa a frente
+            ultima_posicao -= 1
 
         self.vetor[posicao] = valor_a_inserir # Atribuição do valor à posição 
-        self.ultima_posicao += 1 # Incrementa a ultima posição, pois foi add um novo numero
+        self.ultima_posicao += 1 # Incrementa a ultima_posição, pois foi add um novo numero
+
+    def pesquisar(self,valor_a_pesquisar):
+
+        for i in range(self.ultima_posicao + 1):
+
+            if self.vetor[i] == valor_a_pesquisar: # Se encontrar
+                return i
+
+            if self.vetor[i] > valor_a_pesquisar: # Se não encontrar
+                return -1
+        
+            if i == self.ultima_posicao: 
+                return -1
+
+    def excluir(self, valor_a_excluir):
+        posicao = self.pesquisar(valor_a_excluir)
+        if posicao == -1:
+            return -1
+        
+        else:
+            for i in range(posicao,self.ultima_posicao):
+                self.vetor[i] = self.vetor[i + 1]
+
+            self.ultima_posicao -= 1
+
 
 vetor = Vetor_Ordenado(4)
 
@@ -51,6 +76,8 @@ vetor.Inserir(5)
 vetor.Inserir(12)
 vetor.Inserir(10)
 
+vetor.pesquisar(13)
+# vetor.excluir(3)
 vetor.Imprime()
 
 
